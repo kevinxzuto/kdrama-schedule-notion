1e9c339350b116e5520beabff7fe3edbcd180748
kevinxzuto <4521210025@univpancasila.ac.id>
2024-01-11 16:12:54 +0700

    fetch all the data instead only fetch next ep to air and fix update data to notion function

--- index.js
+++ index.js
@@ -13,32 +13,37 @@ const fetchData = async () => {
 
     const fetchDataForShows = tmdb.map(async (shows) => {
         try {
-            const epInfo = await axios.get(`https://api.themoviedb.org/3/tv/${shows.id}?api_key=${apiKey}&language=en-US`)
+            const fetchEpInfo = await axios.get(`https://api.themoviedb.org/3/tv/${shows.id}?api_key=${apiKey}&language=en-US`)
+            const showsProvider = await axios.get(`https://api.themoviedb.org/3/tv/${shows.id}/watch/providers?api_key=${apiKey}`)
+            const fetchSeasonInfo = await axios.get(`https://api.themoviedb.org/3/tv/${shows.id}/season/${fetchEpInfo.data.number_of_seasons}?api_key=${apiKey}&language=en-US`)
+            const seasonInfo = fetchSeasonInfo.data.episodes
+            const FetchDataAgain = seasonInfo.map((shows) => {
 
-            const tmdbSeasonInfo = {
-                "name": epInfo.data.name,
-                "season": epInfo.data.number_of_seasons,
-                "next_airEP": epInfo.data.next_episode_to_air.name,
-                "next_airSeason": epInfo.data.next_episode_to_air.season_number,
-                "next_airDATE": epInfo.data.next_episode_to_air.air_date,
-                "img": `https://image.tmdb.org/t/p/original${epInfo.data.poster_path}`
-            }
+                const tmdbSeasonInfo = {
+                    "id": fetchEpInfo.data.id,
+                    "name": fetchEpInfo.data.name,
+                    "season": fetchEpInfo.data.number_of_seasons,
+                    "next_airEP": `Episode ${shows.episode_number}`,
+                    "next_airSeason": fetchEpInfo.data.next_episode_to_air.season_number,
+                    "next_airDATE": shows.air_date !== null ? shows.air_date : '1970-01-01',
+                    "img": `https://image.tmdb.org/t/p/original${fetchEpInfo.data.poster_path}`,
+                }
 
-            console.log(`Fetched ${epInfo.data.name}.`)
-            return tmdbSeasonInfo
+                console.log(`Fetched ${fetchEpInfo.data.name}.`)
+                return tmdbSeasonInfo
+            })
+            const fetchedShowsData = await Promise.all(FetchDataAgain)
+            tmdbArr.push(...fetchedShowsData)
+
         } catch (error) {
             console.error('Error fetching show data:', error)
             return null
         }
     })
 
-    const fetchedShowsData = await Promise.all(fetchDataForShows)
-    const filteredData = fetchedShowsData.filter(show => show !== null)
-    tmdbArr.push(...filteredData)
 }
 
 const createNotionpage = async () => {
-
     try {
         const existingPages = await notion.databases.query({
             database_id: process.env.NOTION_DATABASE_ID,
@@ -46,90 +51,82 @@ const createNotionpage = async () => {
 
         for (let data of tmdbArr) {
 
-            const matchingEntry = existingPages.results.find((page) => {
-                return (
-                    page.properties.Season.number === data.next_airSeason &&
-                    page.properties.Episode.rich_text[0].text.content === data.next_airEP &&
-                    page.properties.Date.date.start === data.next_airDATE // Check for different date
-                )
-            })
+            const duplicateCheck = existingPages.results.find(page =>
+                page.properties.Name.title[0]?.text.content === data.name &&
+                page.properties.Episode.rich_text[0]?.text.content === data.next_airEP
+            );
 
-            if (!matchingEntry) {
-                const existingEntryWithSameDetails = existingPages.results.find((page) => {
-                    return (
-                        page.properties.Season.number === data.next_airSeason &&
-                        page.properties.Episode.rich_text[0].text.content === data.next_airEP &&
-                        page.properties.Date.date.start !== data.next_airDATE
-                    )
-                })
+            if (duplicateCheck) {
+                const existingPageId = duplicateCheck.id;
 
-                if (existingEntryWithSameDetails) {
-                    console.log(`Entry for ${data.name} Season ${data.next_airSeason} Episode "${data.next_airEP}" with a different date already exists. Updating date...`)
+                const clearResponse = await notion.pages.update({
+                    page_id: existingPageId,
+                    properties: {},
+                });
 
-                    const response = await notion.pages.update({
-                        page_id: existingEntryWithSameDetails.id,
-                        properties: {
-                            Date: {
-                                date: {
-                                    start: data.next_airDATE,
-                                },
-                            },
-                        },
-                    })
-                    console.log(response)
-                } else {
-                    const response = await notion.pages.create({
-                        "parent": {
-                            "type": "database_id",
-                            "database_id": process.env.NOTION_DATABASE_ID
-                        },
-                        "properties": {
-                            "Name": {
-                                "title": [
-                                    {
-                                        "type": "text",
-                                        "text": {
-                                            "content": data.name
-                                        }
-                                    }
-                                ]
-                            },
-                            "Season": {
-                                "number": data.next_airSeason
-                            },
-                            "Episode": {
-                                "rich_text": [
-                                    {
-                                        "type": "text",
-                                        "text": {
-                                            "content": data.next_airEP
-                                        }
-                                    }
-                                ]
-                            },
-                            "Date": {
-                                "date": {
-                                    "start": data.next_airDATE
-                                }
-                            },
-                            "IMG": {
-                                "files": [
-                                    {
-                                        "name": "Poster",
-                                        "external": {
-                                            "url": data.img
-                                        }
-                                    }
-                                ]
-                            },
+                const updateResponse = await notion.pages.update({
+                    page_id: existingPageId,
+                    properties: {
+                        "Date": {
+                            "date": {
+                                "start": data.next_airDATE
+                            }
                         }
-                    })
-                    console.log(response)
-                }
+                    }
+                });
+
+                console.log(`Updated ${data.name}, ${data.next_airEP} with a new Date: ${data.next_airDATE}`);
             } else {
-                console.log(
-                    `Entry for ${data.name} Season ${data.next_airSeason} Episode "${data.next_airEP}" already exists.`
-                )
+                const response = await notion.pages.create({
+                    "parent": {
+                        "type": "database_id",
+                        "database_id": process.env.NOTION_DATABASE_ID
+                    },
+                    "properties": {
+                        "Name": {
+                            "title": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": data.name
+                                    }
+                                }
+                            ]
+                        },
+                        "Season": {
+                            "number": data.next_airSeason
+                        },
+                        "TMDB ID": {
+                            "number": data.id
+                        },
+                        "Episode": {
+                            "rich_text": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": data.next_airEP
+                                    }
+                                }
+                            ]
+                        },
+                        "Date": {
+                            "date": {
+                                "start": data.next_airDATE
+                            }
+                        },
+                        "IMG": {
+                            "files": [
+                                {
+                                    "name": "Poster",
+                                    "external": {
+                                        "url": data.img
+                                    }
+                                }
+                            ]
+                        },
+                    }
+                })
+                console.log(response)
             }
         }
     }
@@ -150,5 +147,5 @@ const fetchDataAndUpdate = async () => {
 fetchDataAndUpdate()
 
 
-const interval = 43200000
+const interval = 3600000
 setInterval(fetchDataAndUpdate, interval)
